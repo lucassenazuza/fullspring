7efbc64fd9fd9aed068ea62e6d87ceb52b9f2920
lucas <lucassenazuza@gmail.com>
2022-03-13 21:50:41 -0300

    first

---
+++ src/test/java/com/project/fullspring/FullspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fullspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
